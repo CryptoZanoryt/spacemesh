bb7d6bd10f716bfc61b03336342ecb7cf2b0aed4
CryptoZanoryt <140786881+CryptoZanoryt@users.noreply.github.com>
2023-07-28 12:39:49 -0700

    Update plot_speed.py

--- plot-speed/plot_speed.py
+++ plot-speed/plot_speed.py
@@ -183,4 +183,4 @@ if first_file is not None and most_recent_complete_file is not None and last_fil
   print(f"Estimated finish time ....................... {recent_etf_string}")
   print(f"Estimated finish date ....................... {efd}")
 else:
-  print("There are not enough files in the directory yet. Will calculate once the first file completes.")
+  print("There are not enough files in the directory yet. Will calculate once the first two files complete.")
