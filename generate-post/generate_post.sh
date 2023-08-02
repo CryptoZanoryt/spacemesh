e3cc9507edb15c423ca0bb628f2c004cf3eb4a77
CryptoZanoryt <140786881+CryptoZanoryt@users.noreply.github.com>
2023-08-02 12:19:24 -0700

    Update generate_post.sh

--- generate-post/generate_post.sh
+++ generate-post/generate_post.sh
@@ -11,8 +11,11 @@
 # Zanoryt <zanoryt@protonmail.com>
 #
 
-PLOT_SPEED_URL="https://github.com/CryptoZanoryt/spacemesh/blob/main/plot-speed/plot_speed.py"
+PLOT_SPEED_URL="https://github.com/CryptoZanoryt/spacemesh/blob/main/plot-speed/smesher-plot-speed.py"
+POSTCLI_VERSION="0.8.11"
 POSTCLI_PATH="/tmp/postcli"
+POSTCLI_FULLPATH="${POSTCLI_PATH}/postcli"
+PLOT_SPEED_FULLPATH="${POSTCLI_PATH}/smesher-plot-speed.py"
 POST_DATA_PATH="/tmp/post-data"
 
 # Update system and install dependencies
@@ -23,12 +26,10 @@ apt install -y nvtop htop screen unzip xxd
 # Download postcli
 rm -rf $POSTCLI_PATH
 mkdir -p $POSTCLI_PATH
-wget https://github.com/spacemeshos/post/releases/download/v0.8.8/postcli-Linux.zip
-unzip -u postcli-Linux.zip -d $POSTCLI_PATH
-rm postcli-Linux.zip
-chmod +x $POSTCLI_PATH/postcli
+wget -O- https://github.com/spacemeshos/post/releases/download/v${POSTCLI_VERSION}/postcli-Linux.zip | unzip -u - -d $POSTCLI_PATH
+chmod +x $POSTCLI_FULLPATH
 
-wget -O $POSTCLI_PATH/plot_speed.py $PLOT_SPEED_URL
+wget -O $PLOT_SPEED_FULL_PATH $PLOT_SPEED_URL
 
 # nodeId in base64 format
 nodeId="URZgMjtU06WgahvNHpvtr89NnB2IIhw2Yo8ZqfZx0ts="
@@ -52,31 +53,26 @@ echo "commitmentAtxId: ${commitmentAtxId}"
 rm -rf $POST_DATA_PATH
 mkdir -p $POST_DATA_PATH
 
-if [ "$numGpus" == "2" ] || [ "$numGpus" == "4" ] || [ "$numGpus" == "6" ] || [ "$numGpus" == "8" ]; then
-  echo "Initializing screen"
-  screen -d -m -S post
-  screen -S post -X exec watch -n 5 python3 $POSTCLI_PATH/plot_speed.py ${POST_DATA_PATH}/
-  screen -s post -X screen -t nvtop
-  screen -S post -p nvtop -X exec nvtop
-  screen -S post -X screen -t htop
-  screen -S post -p htop -X exec htop
+echo "Initializing screen"
+screen -d -m -S post
+screen -S post -X exec watch -n 5 python3 $PLOT_SPEED_FULLPATH ${POST_DATA_PATH}/
+screen -s post -X screen -t nvtop
+screen -S post -p nvtop -X exec nvtop
+screen -S post -X screen -t htop
+screen -S post -p htop -X exec htop
 
-  echo "Generating post files..."
-  for ((i=1; i<=$numGpus; i++))
-  do
-    provider=$((i-1))
-    screen -S post -X screen -t post$provider
-    screen -S post -p post0 -X exec bash -c "$POSTCLI_PATH/postcli -provider $provider -commitmentAtxId $commitmentAtxId -id $id -labelsPerUnit $labelsPerUnit -maxFileSize $maxFileSize -numUnits $numUnits -datadir $POST_DATA_PATH -fromFile $((numUnits*32/numGpus*$provider)) -toFile $((-1+numUnits*32/numGpus*$i)); exec bash"
-  done
+echo "Generating post files..."
+for ((i=1; i<=$numGpus; i++))
+do
+  provider=$((i-1))
+  screen -S post -X screen -t post$provider
+  screen -S post -p post0 -X exec bash -c "$POSTCLI_FULLPATH -provider $provider -commitmentAtxId $commitmentAtxId -id $id -labelsPerUnit $labelsPerUnit -maxFileSize $maxFileSize -numUnits $numUnits -datadir $POST_DATA_PATH -fromFile $((numUnits*32/numGpus*$provider)) -toFile $((-1+numUnits*32/numGpus*$i)); exec bash"
+done
 
-  echo "Started generating the PoST data files."
-  echo ""
-  echo "To attach to the screen session, run:"
-  echo ""
-  echo "  screen -d -r post"
-  echo ""
-  echo "Have fun!"
-else
-  echo "Invalid number of GPUs (must be 2, 4, 6, or 8)"
-  exit 1
-fi
+echo "Started generating the PoST data files."
+echo ""
+echo "To attach to the screen session, run:"
+echo ""
+echo "  screen -d -r post"
+echo ""
+echo "Have fun!"
