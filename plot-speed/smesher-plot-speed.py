40d6f4032b15367e709ce66ecb5ca6dbccec3ae2
CryptoZanoryt <140786881+CryptoZanoryt@users.noreply.github.com>
2023-08-03 01:29:21 -0700

    Update smesher-plot-speed.py
    Assign file_ranges in a better location after postdate is defined

--- plot-speed/smesher-plot-speed.py
+++ plot-speed/smesher-plot-speed.py
@@ -465,7 +465,6 @@ detect_cpu()
 gpus = detect_gpus()
 num_gpus = len(gpus)
 detect_provider()
-file_ranges = [(int(postdata['num_units'] * 32 / num_gpus * i), int(-1 + postdata['num_units'] * 32 / num_gpus * (i + 1))) for i in range(num_gpus)]
 
 if print_header:
   print(f"Smesher Plot Speed v{version} ({github_url})")
@@ -482,6 +481,7 @@ if not os.path.isdir(directory):
     sys.exit(1)
 
 postdata = postdata_metadata()
+file_ranges = [(int(postdata['num_units'] * 32 / num_gpus * i), int(-1 + postdata['num_units'] * 32 / num_gpus * (i + 1))) for i in range(num_gpus)]
 smeshing = {}
 files = postdata_bin_files(directory)
 current_post_size_GiB = calculate_current_post_size_GiB(directory)
