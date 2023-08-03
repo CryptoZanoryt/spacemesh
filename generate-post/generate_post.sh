00ccbcdaef5385915905b40505761477e469956c
CryptoZanoryt <140786881+CryptoZanoryt@users.noreply.github.com>
2023-08-02 18:38:00 -0700

    Update generate_post.sh

--- generate-post/generate_post.sh
+++ generate-post/generate_post.sh
@@ -11,11 +11,10 @@
 # Zanoryt <zanoryt@protonmail.com>
 #
 
-nodeId="URZgMjtU06WgahvNHpvtr89NnB2IIhw2Yo8ZqfZx0ts="  # nodeId in base64 format
-id=$(echo "$nodeId" | base64 -d | xxd -p -c 32 -g 32)  # nodeId in HEX format
-echo "Node ID: ${id}"
-
-commitmentAtxId="9eebff023abb17ccb775c602daade8ed708f0a50d3149a42801184f5b74f2865"
+nodeId=${1:-"511660323b54d3a5a06a1bcd1e9bedafcf4d9c1d88221c36628f19a9f671d2db"}
+commitmentAtxId=${2:-"9eebff023abb17ccb775c602daade8ed708f0a50d3149a42801184f5b74f2865"}
+# id=$(echo "$nodeId" | base64 -d | xxd -p -c 32 -g 32)        # nodeId in HEX format
+echo "Node ID: ${nodeId}"
 echo "commitmentAtxId: ${commitmentAtxId}"
 
 # 2 or 4
@@ -35,9 +34,7 @@ PLOT_SPEED_FULLPATH="/tmp/smesher-plot-speed.py"
 POST_DATA_PATH="/tmp/post-data"
 
 # Update system and install dependencies
-apt update
-apt install -y clinfo
-apt install -y nvtop htop screen unzip xxd python3 tmux
+apt update && apt install -y clinfo nvtop htop screen unzip xxd python3 tmux
 
 # Download postcli
 rm -rf $POSTCLI_PATH
@@ -65,7 +62,7 @@ for ((i=1; i<=$numGpus; i++))
 do
   provider=$((i-1))
   tmux new-window -t post -n post$provider
-  tmux send-keys -t post:post$provider "$POSTCLI_FULLPATH -provider $provider -commitmentAtxId $commitmentAtxId -id $id -labelsPerUnit $labelsPerUnit -maxFileSize $maxFileSize -numUnits $numUnits -datadir $POST_DATA_PATH -fromFile $((numUnits*32/numGpus*$provider)) -toFile $((-1+numUnits*32/numGpus*$i)); exec bash" Enter
+  tmux send-keys -t post:post$provider "$POSTCLI_FULLPATH -provider $provider -commitmentAtxId $commitmentAtxId -id $nodeId -labelsPerUnit $labelsPerUnit -maxFileSize $maxFileSize -numUnits $numUnits -datadir $POST_DATA_PATH -fromFile $((numUnits*32/numGpus*$provider)) -toFile $((-1+numUnits*32/numGpus*$i)); exec bash" Enter
 done
 
 echo "Started generating the PoST data files."
